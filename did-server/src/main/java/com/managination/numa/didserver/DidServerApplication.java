3bb3ce292cbec9255358267f36631a483184226e
Micha Roon <micha.roon@managination.com>
2026-03-24 17:17:10 +0100

    refactor to modules

---
+++ did-server/src/main/java/com/managination/numa/didserver/DidServerApplication.java
@@ -0,0 +1,11 @@
+package com.managination.numa.didserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidServerApplication {
+   public static void main(String[] args) {
+      SpringApplication.run(DidServerApplication.class, args);
+   }
+}
